a25695842d6a3cca8ebdd764acc531e8067733f8
pradeeppatwal06 <pradeeppatwal06@gmail.com>
2022-05-16 18:44:48 +0530

    first commit

---
+++ src/main/java/com/pradeep/springjenkinsgit/SpringJenkinsGitApplication.java
@@ -0,0 +1,13 @@
+package com.pradeep.springjenkinsgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pradeep/springjenkinsgit/SpringJenkinsGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradeep.springjenkinsgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
