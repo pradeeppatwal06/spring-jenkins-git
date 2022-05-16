0c84e259ee309bb852ad6f7bbe32121a9a5e1f47
pradeeppatwal06 <pradeeppatwal06@gmail.com>
2022-05-16 21:44:14 +0530

    third commit

--- src/main/java/com/pradeep/springjenkinsgit/controller/SpringJenkinsGitController.java
+++ src/main/java/com/pradeep/springjenkinsgit/controller/SpringJenkinsGitController.java
@@ -11,6 +11,6 @@ public class SpringJenkinsGitController {
 
     @GetMapping("/get/{name}")
     public String get(@PathVariable("name") String name) {
-        return "Hello " +name;
+        return "Hello, how are you" +name;
     }
 }
