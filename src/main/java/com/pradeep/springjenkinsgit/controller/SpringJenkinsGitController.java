56ced563f9c50463f3b16decbbcf1e11e7d07c1e
pradeeppatwal06 <pradeeppatwal06@gmail.com>
2022-05-16 18:53:33 +0530

    controller changes

---
+++ src/main/java/com/pradeep/springjenkinsgit/controller/SpringJenkinsGitController.java
@@ -0,0 +1,16 @@
+package com.pradeep.springjenkinsgit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jenkins")
+public class SpringJenkinsGitController {
+
+    @GetMapping("/get/{name}")
+    public String get(@PathVariable("name") String name) {
+        return "Hello " +name;
+    }
+}
